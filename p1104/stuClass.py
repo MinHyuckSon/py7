692836f9054e1af3831c07498046afb7ab0273bc
minhtukson <sonmin458@gmail.com>
2025-11-04 18:32:41 +0900

    클래스 생성자

--- p1104/stuClass.py
+++ p1104/stuClass.py
@@ -1,5 +1,29 @@
-class CStus:
-    
-    
-    def_init_(self):
+class CStus: 
+    stuTotal = 0
+    def __init__(self,_stuNum,_stuName,_stuKor,_stuEng,_stuMath):
+        self.stuNum     = _stuNum
+        self.stuName    = _stuName
+        self.stuKor     = _stuKor
+        self.stuEng     = _stuEng
+        self.stuMath    = _stuMath
+        self.stuTotal   = _stuKor + _stuEng + _stuMath
+        self.stuAvg     = stuTotal/3
+        self.rank       = 0
+
+    def Set_Info(self,_stuNum,_stuName,_stuKor,_stuEng,_stuMath):
+        self.stuNum     = _stuNum
+        self.stuName    = _stuName
+        self.stuKor     = _stuKor
+        self.stuEng     = _stuEng
+        self.stuMath    = _stuMath
         
+
+
+StuInfo = CStus()
+
+StuInfo.Set_Info(input("학생번호를 입력하세요: "),input("학생이름을 입력하세요"),\
+                int(input("국어점수를 입력하세요: ")),int(input("영어점수를 입력하세요: ")),\
+                   int(input("수학점수를 입력하세요: ")))
+
+print(StuInfo.stuNum,StuInfo.stuName,StuInfo.stuKor,StuInfo.stuEng,\
+    StuInfo.stuMath)
